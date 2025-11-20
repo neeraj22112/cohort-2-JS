56df24f54893d54313a4391e121965800848768d
Neeraj Bhardwaj <neerajbhardwaj2211@gmail.com>
2025-11-20 17:58:45 +0530

    Download effect

---
+++ Download effect/script.js
@@ -0,0 +1,22 @@
+var grow = 0;
+var btn = document.querySelector("button");
+var h2 = document.querySelector("h2");
+var inner = document.querySelector(".inner");
+
+btn.addEventListener("click", function () {
+  btn.style.pointerEvents = "none";
+
+  var num = 50 + Math.floor(Math.random() * 50);
+
+  var int = setInterval(() => {
+    grow++;
+    h2.innerHTML = grow + "%";
+    inner.style.width = grow + "%";
+  }, num);
+
+  setTimeout(() => {
+    clearInterval(int);
+    btn.innerHTML = "Downloaded";
+    btn.style.opacity = 0.5;
+  }, num * 100);
+});
