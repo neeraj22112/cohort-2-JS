0762d5870cfcd7ace0caf3896f7de95c0ababb7f
Neeraj Bhardwaj <neerajbhardwaj2211@gmail.com>
2025-11-27 13:40:22 +0530

    spotlight effect

---
+++ spotlight effect/script.js
@@ -0,0 +1,4 @@
+addEventListener("mousemove", function (e) {
+  document.documentElement.style.setProperty("--x", e.clientX + "px");
+  document.documentElement.style.setProperty("--y", e.clientY + "px");
+});
